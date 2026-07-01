d5df5240f6b5ddd9b1c723e6181a8e3633bd1083
Bhairab Sarkar <157372465+Bhairab66@users.noreply.github.com>
2026-07-01 07:24:41 +0530

    config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "7053545607"
+    sudo_users = "7053545607"
+    GROUP_ID = -1003659707205
+    TOKEN = "7712201855:AAEEqz6uQRzWjw6fFygXiBgDMfIgjCl96x0"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
     BOT_USERNAME = "Collect_Em_AllBot"
     CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    api_id = "36237034"
+    api_hash = "31c376a5c49eabb8426df4d1f21a5d52"
 
     
 class Production(Config):
